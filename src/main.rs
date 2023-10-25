d38d0dddee883ac914b6ea8a2260bee144d926fa
DevCorvus <devcorvus.contact@gmail.com>
2023-10-25 17:21:05 -0400

    feat: add clap for a better cli and implement json files support

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,38 @@ mod lexer;
 mod parser;
 mod utils;
 
-use std::env;
-use utils::parse_json;
+use clap::{ArgGroup, Parser};
+use std::{fs, path::PathBuf};
+use utils::parse_json_and_print;
+
+/// Crusty JSON parser
+#[derive(Parser)]
+#[clap(group = ArgGroup::new("input").required(true).args(&["json", "file"]))]
+struct Args {
+    /// In-line json to parse from
+    #[clap(conflicts_with = "file")]
+    json: Option<String>,
+
+    /// Path to json file to parse from
+    #[clap(short, long, conflicts_with = "json")]
+    file: Option<PathBuf>,
+}
 
 fn main() {
-    let mut args = env::args();
-    args.next(); // Skip first argument
+    let args = Args::parse();
 
-    if let Some(input) = args.next() {
-        match parse_json(input) {
-            Ok((tokens, json)) => {
-                println!("Tokens: {:?}", tokens);
-                println!("JSON: {:?}", json);
+    if let Some(json) = args.json {
+        parse_json_and_print(json);
+    } else if let Some(file_path) = args.file {
+        match fs::read_to_string(file_path) {
+            Ok(file_content) => {
+                parse_json_and_print(file_content);
             }
             Err(err) => {
-                println!("Error: {}", err);
+                eprintln!("{}", err);
             }
-        };
+        }
     } else {
-        println!("Missing input");
+        unreachable!();
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -3,8 +3,20 @@ use crate::{
     parser::{parser, JsonValue},
 };
 
-pub fn parse_json(text: String) -> anyhow::Result<(Vec<JsonToken>, JsonValue)> {
+fn parse_json(text: String) -> anyhow::Result<(Vec<JsonToken>, JsonValue)> {
     let tokens = lexer(text)?;
     let json = parser(&tokens)?;
     return Ok((tokens, json));
+}
+
+pub fn parse_json_and_print(text: String) {
+    match parse_json(text) {
+        Ok((tokens, json)) => {
+            println!("Tokens: {:?}", tokens);
+            println!("JSON: {:?}", json);
+        }
+        Err(err) => {
+            eprintln!("Error: {}", err);
+        }
+    };
 }
